275193219b64124410e823375e1cc48a1046990a
diukin <petr.diukin@gmail.com>
2022-10-18 20:11:25 +0500

    Remove ignored files

---
+++ Paraglider.AspNetCore.Identity/Paraglider.AspNetCore.Identity.Web/Endpoints/SecurityEndpoints/Controllers/ExternalAuthorizeController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Paraglider.AspNetCore.Identity.Web.Endpoints.SecurityEndpoints.Controllers
+{
+    public class ExternalAuthorizeController : Controller
+    {
+    }
+}
