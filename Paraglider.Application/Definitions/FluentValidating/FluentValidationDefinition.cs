e35f0ab7a1cc3f282c686eee350d4da8f94296d6
diukin <petr.diukin@gmail.com>
2022-11-18 22:16:43 +0500

    localised messages from fluent validation

--- Paraglider.Application/Definitions/FluentValidating/FluentValidationDefinition.cs
+++ Paraglider.Application/Definitions/FluentValidating/FluentValidationDefinition.cs
@@ -1,6 +1,7 @@
 ﻿using FluentValidation;
 using Paraglider.API.Features.Authorization.Commands;
 using Paraglider.Web.Definitions.Base;
+using System.Globalization;
 
 namespace Paraglider.Web.Definitions.FluentValidating;
 
@@ -9,7 +10,7 @@ public class FluentValidationDefinition : AppDefinition
     public override void ConfigureServices(IServiceCollection services, IConfiguration configuration)
     {
         services.AddScoped<IValidator<BasicAuthRequest>, BasicAuthRequestValidator>();
-
+        ValidatorOptions.Global.LanguageManager.Culture = new CultureInfo("en");
         services.AddValidatorsFromAssembly(typeof(Program).Assembly);
     }
 }
