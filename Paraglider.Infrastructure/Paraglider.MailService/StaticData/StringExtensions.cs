f67def08bb02324b7350fabd2e00527a8749503b
petr.diukin <73099485+diukin9@users.noreply.github.com>
2022-11-20 16:44:23 +0500

    added mail service (#16)
    * added mail service
    
    * code_review

---
+++ Paraglider.Infrastructure/Paraglider.MailService/StaticData/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Paraglider.MailService;
+
+public static partial class StringExtensions
+{
+    public static bool IsEmail(this string value)
+    {
+        var regex = EmailRegex();
+        return regex.Match(value).Success;
+    }
+
+    [GeneratedRegex("^([\\w\\.\\-]+)@([\\w\\-]+)((\\.(\\w){2,3})+)$")]
+    private static partial Regex EmailRegex();
+}
---
+++ Paraglider.Infrastructure/Paraglider.MailService/StaticData/StringHelper.cs
@@ -0,0 +1,9 @@
+﻿namespace Paraglider.MailService;
+
+public static class StringHelper
+{
+    public static bool CheckForNullOrEmpty(params string[] values)
+    {
+        return values.Any(string.IsNullOrEmpty);
+    }
+}
