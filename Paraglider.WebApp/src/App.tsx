7ef7779f7c660b9544268a35e1ddcce1998fc3b9
syomcha <semen2k1@mail.ru>
2022-11-27 15:44:01 +0500

    Add main route

--- Paraglider.WebApp/src/App.tsx
+++ Paraglider.WebApp/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
+import { router } from "./routes";
 import { GlobalStyle, theme } from "./styles/theme";
 
 export const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <>App</>
+      <RouterProvider router={router} />
       <GlobalStyle />
     </ThemeProvider>
   );
---
+++ Paraglider.WebApp/src/routes/router.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import { Main } from "../features/Main";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    children: [
+      {
+        index: true,
+        element: <Main />,
+      },
+    ],
+  },
+]);
