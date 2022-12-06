4d4579c8c246e94184be61716958e9c6c34ded98
petr.diukin <petr.diukin@urfu.me>
2022-12-06 09:52:42 +0500

    added attribute validator

---
+++ Paraglider.Infrastructure/Paraglider.Infrastructure.Common/Extensions/ValidationResultExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Paraglider.Infrastructure.Common.Extensions;
+
+public static class ValidationResultExtensions
+{
+    public static bool IsValid(this ValidationResult result)
+    {
+        return string.IsNullOrEmpty(result.ErrorMessage); 
+    }
+}
