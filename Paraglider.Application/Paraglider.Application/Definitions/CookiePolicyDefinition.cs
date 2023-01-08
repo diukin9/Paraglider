5de6ab98433a20583ca8b61fb39d4dd994b3b9d8
Maksim.Surimov <MaxxLorge@gmail.com>
2023-01-09 02:50:20 +0500

    Add CookiePolicyDefinition

---
+++ Paraglider.Application/Paraglider.Application/Definitions/CookiePolicyDefinition.cs
@@ -0,0 +1,20 @@
+using Paraglider.Application.Extensions;
+using Paraglider.Infrastructure.Common.AppDefinition;
+using Paraglider.Infrastructure.Common.Attributes;
+
+namespace Paraglider.Application.Definitions;
+
+[CallingOrder(0)]
+public class CookiePolicyDefinition : AppDefinition
+{
+    public override void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+    {
+        services.ConfigureNonBreakingSameSiteCookies();
+    }
+
+    public override void ConfigureApplication(WebApplication app, IWebHostEnvironment env)
+    {
+        // Add this before any other middleware that might write cookies
+        app.UseCookiePolicy();
+    }
+}
