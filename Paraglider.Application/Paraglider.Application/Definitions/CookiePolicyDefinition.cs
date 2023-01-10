76383f9652cb8c97b0f5c9ae3ace3ed0e0e10383
petr.diukin <petr.diukin@urfu.me>
2023-01-10 16:40:26 +0500

    change same site mode

--- Paraglider.Application/Paraglider.Application/Definitions/CookiePolicyDefinition.cs
+++ Paraglider.Application/Paraglider.Application/Definitions/CookiePolicyDefinition.cs
@@ -9,9 +9,11 @@ public class CookiePolicyDefinition : AppDefinition
     public override void ConfigureApplication(WebApplication app, IWebHostEnvironment env)
     {
         // Add this before any other middleware that might write cookies
-        //app.UseCookiePolicy(new CookiePolicyOptions()
-        //{
-        //    MinimumSameSitePolicy = SameSiteMode.None
-        //});
+        app.UseCookiePolicy(new CookiePolicyOptions()
+        {
+            MinimumSameSitePolicy = SameSiteMode.None,
+            OnAppendCookie = options => options.CookieOptions.SameSite = SameSiteMode.None
+        });
     }
+}
 }
